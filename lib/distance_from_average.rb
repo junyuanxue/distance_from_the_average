57c770bdda5b425a9be3178f028b187da400c344
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-24 11:49:37 +0000

    Passing the first test

--- lib/distance_from_average.rb
+++ lib/distance_from_average.rb
@@ -1,3 +1,6 @@
 def distance_from_average(arr)
-  
+  output = []
+  average = arr.inject {|sum, n| sum + n}.to_f / arr.size
+  arr.each {|n| output << (average - n).round(1) }
+  output
 end
--- spec/distance_from_average_spec.rb
+++ spec/distance_from_average_spec.rb
@@ -1,7 +1,9 @@
 require "distance_from_average"
 
 describe "distance_from_average" do
-  it "didn't return distance from average" do
+  it "didn't return the correct distance from average" do
     expect(distance_from_average([55, 95, 62, 36, 48])).to eq [4.2, -35.8, -2.8, 23.2, 11.2]
   end
+
+
 end
